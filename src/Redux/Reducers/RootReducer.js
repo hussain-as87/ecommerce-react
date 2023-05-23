fd125fa5ba0969f7b6c053103ba08993a36441f9
Hussain ASelmia <hussain87nyn@gmail.com>
2023-05-23 14:32:16 +0300

    add product redux and take the first step of building products components

--- src/Redux/Reducers/RootReducer.js
+++ src/Redux/Reducers/RootReducer.js
@@ -2,10 +2,12 @@ import {combineReducers} from "redux"
 import CategoryReducer from "./CategoryReducer"
 import BrandReducer from "./BrandReducer";
 import SubcategoryReducer from "./SubcategoryReducer";
+import ProductReducer from "./ProductReducer";
 
 const RootReducer = combineReducers({
     categories:CategoryReducer,
     brands:BrandReducer,
-    subcategories:SubcategoryReducer
+    subcategories:SubcategoryReducer,
+    products:ProductReducer
 })
 export default RootReducer
