51eb993eb750da7717b04b42fb0ac366d89bb0a2
hussain <hussain87nyn@gmail.com>
2023-07-02 09:27:36 +0300

    Refactor components, controllers, and reducers for improved functionality and clarity.

--- src/Components/Rate/RateItem.js
+++ src/Components/Rate/RateItem.js
@@ -1,8 +1,15 @@
 import React, {useState} from 'react';
 import {Row, Col, Button, Modal, Form, FloatingLabel} from 'react-bootstrap';
-import {StarFill, Trash2, Pin, Pencil, Save, SdCardFill, XCircleFill} from "react-bootstrap-icons"
+import {
+    StarFill,
+    Trash2,
+    Pencil,
+    Backspace,
+    Check2Circle
+} from "react-bootstrap-icons"
 import ReactStars from "react-rating-stars-component";
 import {DestroyReview, EditReview} from "../../Controllers/ReviewController";
+import {GetLoggedUser} from "../../Controllers/UserController";
 
 const RateItem = ({review}) => {
     const {
@@ -23,66 +30,71 @@ const RateItem = ({review}) => {
         onChange: handlerOnChangeRating
     };
     const {deleteHandler} = DestroyReview(review._id)
+    const {user} = GetLoggedUser()
     return (
         <div>
             <Row className="mt-3">
-                <Col className="d-felx me-5">
-                    <div className="rate-name  d-inline ms-2">{review?.user?.name}</div>
-                    <StarFill className="text-warning"/>
-                    <div className="cat-rate  d-inline  p-1 pt-2">{review?.ratings}</div>
+                <Col className="d-felx align-items-center">
+                    <div className="rate-name  d-inline ms-2 text-primary">{review?.user?.name}</div>
+                    <StarFill className="text-warning ms-2"/>
+                    <span className="ml-2 text-warning">{review?.ratings}</span>
                 </Col>
-                <Col className="text-end">
+                {user?.data?._id === review?.user?._id && (<Col className="text-end">
                     <Button variant="outline-warning" size="sm" className="me-2" onClick={() => setShowModal(true)}>
                         <Pencil/>
                     </Button>
+                    {/* Edit Modal */}
+                    {showModal && (
+                        <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Edit Review</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form.Group className="d-flex align-items-center">
+                                    <ReactStars {...setting} />
+                                    {data.ratings > 2.5 ? <span className="ml-2 text-success">{data.ratings}</span>
+                                        : <span className="ml-2 text-danger">{data.ratings}</span>}
+                                </Form.Group>
+                                <Form.Group controlId="editTitle">
+                                    <FloatingLabel controlId="title" label="Title">
+                                        <Form.Control
+                                            id="title"
+                                            name="title"
+                                            as="textarea"
+                                            placeholder="Enter the description"
+                                            className={errors.some(error => error.param === "title") && 'is-invalid'}
+                                            style={{height: '100px'}}
+                                            value={data.title}
+                                            onChange={handlerOnChangeInput}
+                                        /> {errors.some(error => error.param === "title") &&
+                                        <Form.Text className="text-danger">
+                                            {errors.find(error => error.param === "title").msg}
+                                        </Form.Text>}
+                                    </FloatingLabel>
+                                </Form.Group>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="outline-secondary" onClick={() => setShowModal(false)}>
+                                    <Backspace/>
+                                </Button>
+                                <Button variant="outline-primary" type="submit" onClick={UpdateHandler}>
+                                    <Check2Circle/>
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+                    )}
                     <Button variant="outline-danger" size="sm" onClick={deleteHandler}>
                         <Trash2/>
                     </Button>
-                </Col>
+                </Col>)}
+
             </Row>
             <Row className="border-bottom mx-2">
                 <Col className="d-felx me-4 pb-2">
                     <div className="rate-description  d-inline ms-2">{review?.title}</div>
                 </Col>
             </Row>
-            {/* Edit Modal */}
-            {showModal && (
-                <Modal show={showModal} onHide={() => setShowModal(false)} centered>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Edit Review</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form.Group>
-                            <ReactStars {...setting} />
-                        </Form.Group>
-                        <Form.Group controlId="editTitle">
-                            <FloatingLabel controlId="title" label="Title">
-                                <Form.Control
-                                    id="title"
-                                    name="title"
-                                    as="textarea"
-                                    placeholder="Enter the description"
-                                    className={errors.some(error => error.param === "title") && 'is-invalid'}
-                                    style={{height: '100px'}}
-                                    value={data.title}
-                                    onChange={handlerOnChangeInput}
-                                /> {errors.some(error => error.param === "title") &&
-                                <Form.Text className="text-danger">
-                                    {errors.find(error => error.param === "title").msg}
-                                </Form.Text>}
-                            </FloatingLabel>
-                        </Form.Group>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="outline-secondary" onClick={() => setShowModal(false)}>
-                            <XCircleFill/>
-                        </Button>
-                        <Button variant="outline-primary" type="submit" onClick={UpdateHandler}>
-                            <SdCardFill/>
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            )}
+
         </div>
     );
 };
