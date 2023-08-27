197ddd107c54506ebb8d40534cb55670377ffd97
hussain <hussain87nyn@gmail.com>
2023-08-27 12:14:15 +0300

    Update file: - src/Components/User/UserProfile.js

--- src/Components/User/UserProfile.js
+++ src/Components/User/UserProfile.js
@@ -1,110 +1,104 @@
 import React from 'react';
-import {Row, Col, Spinner, Form, Button} from 'react-bootstrap';
+import {Row, Col, Spinner, Form, Button, Card, Table, Container} from 'react-bootstrap';
 import {ChangeUserPassword, GetLoggedUser} from '../../Controllers/UserController';
-import {faEdit} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import AdminBrands from "../Admin/Brands/AdminBrands";
+import {Pencil, Pin} from "react-bootstrap-icons";
 
 const UserProfile = () => {
     const {user, loading} = GetLoggedUser();
     const {handleSubmit, handlerOnChangeInput, isPress} = ChangeUserPassword()
     return (
-        <div className="pb-3">
-            <h2 className="admin-content-text">Profile</h2>
-
-            {!loading ? (
-                user?.data && (
-                    <div className="user-address-card my-3 px-2">
-                        <Row className="d-flex justify-content-between pt-2">
-                            <Col xs={12} className="d-flex">
-                                <div className="p-2">Name:</div>
-                                <div className="p-1 item-delete-edit">{user.data.name}{''} <FontAwesomeIcon
-                                    icon={faEdit}></FontAwesomeIcon></div>
-                            </Col>
-                            <Col xs={12} className="d-flex">
-                                <div className="p-2">Phone number:</div>
-                                <div className="p-1 item-delete-edit">
-                                    {user.data.phone || 'No Phone Number'}
-                                </div>
-                            </Col>
-                            <Col xs={12} className="d-flex">
-                                <div className="p-2">Email-Address:</div>
-                                <div className="p-1 item-delete-edit">{user.data.email}</div>
-                            </Col>
-                        </Row>
-
-                        <Row className="mt-5">
-                            <Col xs={10} sm={8} md={8} className="mx-auto">
-                                <h2 className="text-center text-dark">Change Password</h2>
-                                <hr/>
-                                <Form onSubmit={handleSubmit}>
-                                    <Form.Group className="text-center">
-                                        <Form.Label>Old Password</Form.Label>
-                                        <Form.Control
-                                            type="password"
-                                            name="currentPassword"
-                                            placeholder="○○○○○○○○○"
-                                            style={{textAlign: 'center'}}
-                                            onChange={handlerOnChangeInput}
-                                        />
-                                        <Form.Text></Form.Text>
-                                    </Form.Group>
-
-                                    <Form.Group className="text-center">
-                                        <Form.Label>New Password</Form.Label>
-                                        <Form.Control
-                                            type="password"
-                                            name="password"
-                                            placeholder="○○○○○○○○○"
-                                            style={{textAlign: 'center'}}
-                                            onChange={handlerOnChangeInput}
-                                        />
-                                        <Form.Text></Form.Text>
-                                    </Form.Group>
-
-                                    <Form.Group className="text-center">
-                                        <Form.Label>Confirm New Password</Form.Label>
-                                        <Form.Control
-                                            type="password"
-                                            name="passwordConfirm"
-                                            placeholder="○○○○○○○○○"
-                                            style={{textAlign: 'center'}}
-                                            onChange={handlerOnChangeInput}
-                                        />
-                                        <Form.Text></Form.Text>
-                                    </Form.Group>
-                                    <div className="text-center">
-                                        <Button
-                                            variant="outline-primary"
-                                            type="submit"
-                                            className="mt-3"
-                                        >
-                                            {isPress ? (
-                                                <Spinner
-                                                    as="span"
-                                                    animation="border"
-                                                    size="sm"
-                                                    role="status"
-                                                    aria-hidden="true"
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title className="">Profile</Card.Title>
+                    {!loading ? (
+                        user?.data && (
+                            <div>
+                                <Row className="d-flex justify-content-between pt-2 text-center">
+                                    <Col xs={12} className="d-block">
+                                        <div className="p-2">Name: <b className="text-primary">{user.data.name}</b>{' '}<Pencil size={15}/></div>
+                                        <div className="p-2">Phone number: <b className="text-primary">{user.data.phone || 'No Phone Number'}</b>
+                                        </div>
+                                        <div className="p-2">Email-Address: <b className="text-primary">{user.data.email}</b></div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={10} sm={8} md={8} className="mx-auto mt-4">
+                                        <h2 className="text-center">Change Password</h2>
+                                        <hr/>
+                                        <Form onSubmit={handleSubmit}>
+                                            <Form.Group className="text-center">
+                                                <Form.Label>Old Password</Form.Label>
+                                                <Form.Control
+                                                    type="password"
+                                                    name="currentPassword"
+                                                    placeholder="○○○○○○○○○"
+                                                    style={{textAlign: 'center'}}
+                                                    onChange={handlerOnChangeInput}
                                                 />
-                                            ) : (
-                                                "Submit")}
-                                        </Button>
-                                    </div>
-                                </Form>
-                            </Col>
-                        </Row>
-                    </div>
-                )
-            ) : (
-                <div className="text-center"><Spinner
-                    animation="border"
-                    variant="secondary"
-                    style={{width: '100px', height: '100px'}}
-                />
-                </div>
+                                                <Form.Text></Form.Text>
+                                            </Form.Group>
 
-            )}
-        </div>
+                                            <Form.Group className="text-center">
+                                                <Form.Label>New Password</Form.Label>
+                                                <Form.Control
+                                                    type="password"
+                                                    name="password"
+                                                    placeholder="○○○○○○○○○"
+                                                    style={{textAlign: 'center'}}
+                                                    onChange={handlerOnChangeInput}
+                                                />
+                                                <Form.Text></Form.Text>
+                                            </Form.Group>
+
+                                            <Form.Group className="text-center">
+                                                <Form.Label>Confirm New Password</Form.Label>
+                                                <Form.Control
+                                                    type="password"
+                                                    name="passwordConfirm"
+                                                    placeholder="○○○○○○○○○"
+                                                    style={{textAlign: 'center'}}
+                                                    onChange={handlerOnChangeInput}
+                                                />
+                                                <Form.Text></Form.Text>
+                                            </Form.Group>
+                                            <div className="text-center">
+                                                <Button
+                                                    variant="outline-primary"
+                                                    type="submit"
+                                                    className="mt-3"
+                                                >
+                                                    {isPress ? (
+                                                        <Spinner
+                                                            as="span"
+                                                            animation="border"
+                                                            size="sm"
+                                                            role="status"
+                                                            aria-hidden="true"
+                                                        />
+                                                    ) : (
+                                                        "Submit")}
+                                                </Button>
+                                            </div>
+                                        </Form>
+                                    </Col>
+                                </Row>
+                            </div>
+                        )
+                    ) : (
+                        <div className="text-center"><Spinner
+                            animation="border"
+                            variant="secondary"
+                            style={{width: '100px', height: '100px'}}
+                        />
+                        </div>
+
+                    )}
+                </Card.Body>
+            </Card>
+        </Container>
     );
 };
 
